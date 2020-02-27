c22ca2978f4ebead316bb919166e20d83f995790
Michael <michael_kirsch>
2020-02-27 18:17:35 +0100

    begin shaderloader

--- src/Engine/ShaderLoader.h
+++ src/Engine/ShaderLoader.h
@@ -3,15 +3,29 @@
 #pragma once
 
 #include <string>
+#include <vector>
+#include "glad/glad.h"
+
+enum class SHADERTYPE{
+    VERTEX,GEOMETRY,FRAGMENT
+};
+
+struct ShaderObject
+{
+    std::string name;
+    SHADERTYPE;
+};
+
+
 class ShaderLoader {
 public:
-    enum SHADERTYPE{
-        VERTEX,GEOMETRY,FRAGMENT
-    };
     ShaderLoader(std::string path_to_shaderfolder);
-
+    unsigned int loadAndCompileShader(ShaderObject obj);
+    unsigned int loadAndCompileShader(std::string name, SHADERTYPE type);
+    unsigned int createProgram(std::vector<ShaderObject> shaders_to_attach);
+    unsigned int createProgram(std::vector<unsigned int> shaders);
+    void useProgramm(unsigned int id){glUseProgram(id);};
     ~ShaderLoader() = default;
-
 private:
 protected:
 };
